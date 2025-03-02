5f0574de779e28f164bb02e66d7a716d33b2e8f3
Guilherme\Guilherme <guilhermepllay157@gmail.com>
2025-03-02 00:11:50 -0300

    first commit

---
+++ src/main/java/br/com/guilherme/primeiro_projeto_spring_boot/controller/PrimeiroController.java
@@ -0,0 +1,9 @@
+package br.com.guilherme.primeiro_projeto_spring_boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/primeiroController")
+public class PrimeiroController {
+}
