4c9cec4a1d07fa0a45ceeec4ebb5e66789dcbf2a
Guilherme\Guilherme <guilhermepllay157@gmail.com>
2025-03-02 18:43:26 -0300

    Primeiro controller

--- src/main/java/br/com/guilherme/primeiro_projeto_spring_boot/controller/PrimeiroController.java
+++ src/main/java/br/com/guilherme/primeiro_projeto_spring_boot/controller/PrimeiroController.java
@@ -1,9 +1,15 @@
 package br.com.guilherme.primeiro_projeto_spring_boot.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/primeiroController")
 public class PrimeiroController {
+
+    @GetMapping("primeiroMetodo")
+    public String primeiroMetodo(){
+        return "meu primeiro metodo";
+    }
 }
